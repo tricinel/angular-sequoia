eb89c498c1cfc071cf6a4c8eabc61215408dc6d0
tricinel <bogdan@blazardesign.com>
2015-11-12 16:43:33 +0100

    Fix travis tests not existing

--- gulpfile.js
+++ gulpfile.js
@@ -74,6 +74,6 @@ gulp.task('default', ['js', 'scss', 'watch']);
 
 gulp.task('build', ['scripts', 'styles']);
 
-gulp.task('test', ['default'], function(done) {
-  new karmaServer({configFile: __dirname + '/karma.conf.js', singleRun: false, autoWatch: true},done).start();
+gulp.task('test', ['js', 'scss'], function(done) {
+  new karmaServer({configFile: __dirname + '/karma.conf.js', singleRun: true, autoWatch: false},done).start();
 });
