4d213f197fa15d48f7456d2fe611198ce922ef48
tricinel <bogdan@blazardesign.com>
2015-11-19 11:14:37 +0100

    Add changelog script to generate the CHANGELOG.md #nochangelog

---
+++ changelog.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+changelog='CHANGELOG.md'
+
+if [ ! -z $1 ] || [ ! -z $2 ]
+then
+  currenttag=$2
+  lasttag=$1
+else
+  currenttag=$(git describe --tags --abbrev=0)
+  lasttag=$(git describe --tags --abbrev=0 ${currenttag}^)
+fi
+
+changes=$(git log --no-walk ${lasttag}...${currenttag} --pretty=format:"%s (%an)" | awk '!(/Bump/ || /Merge/ || /Build/ || /#nochangelog/)')
+
+echo "----> Appending changes since ${lasttag} to the changelog."
+
+echo -e "###${currenttag}\n___\n\n${changes}\n\n$(cat ${changelog})" > ${changelog}
+
+echo "----> Done."
