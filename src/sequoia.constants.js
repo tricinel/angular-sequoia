8af3ae86a335c6ce42baee3e5ce6209466148ba0
tricinel <bogdan@blazardesign.com>
2015-11-20 13:11:09 +0100

    Add constants (contain buttons and nodeTemplate)

---
+++ src/sequoia.constants.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  var buttons = {
+    edit: 'Edit',
+    select: 'Select',
+    deselect: 'Deselect',
+    goToSubitems: 'Go to subitems',
+    addSubitems: 'Add subitems',
+    addNode: 'Add node',
+    remove: 'Delete',
+    done: 'Done',
+    searchClear: '&times;',
+    showSelected: 'Show selected',
+    hideSelected: 'Hide selected',
+    backToList: 'Back to list'
+  };
+
+  var nodeTemplate = {
+    id: '_id',
+    nodes: 'nodes',
+    title: 'title'
+  };
+
+  angular.module('ngSequoia')
+    .constant('BUTTONS', buttons)
+    .constant('NODE_TEMPLATE', nodeTemplate);
+
+})();
