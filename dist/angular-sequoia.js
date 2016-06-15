062df11a13ada48f15fa0c08fe1834f882ff4c0c
tricinel <bogdan@blazardesign.com>
2016-06-15 12:24:54 +0200

    Release v0.3.0

--- dist/angular-sequoia.js
+++ dist/angular-sequoia.js
@@ -104,7 +104,7 @@
 (function() {
   'use strict';
 
-  function sequoiaTreeDirective(Tree, BUTTONS, DEFAULT_OPTIONS, SORTABLE_OPTIONS){
+  function sequoiaTreeDirective(Tree, Utils, BUTTONS, DEFAULT_OPTIONS, SORTABLE_OPTIONS){
 
     return {
       restrict: 'AE',
@@ -118,10 +118,6 @@
         'path': '=?sequoiaTreePath'
       },
       link: function(scope) {
-        function initialiseModel() {
-          scope.model = scope.isMultiSelect ? _.isArray(scope.model) ? scope.model : [] : _.isString(scope.model) ? scope.model : '';
-        }
-
         function init() {
           /* Set the default options*/
           scope.options = _.defaults(scope.options || {}, DEFAULT_OPTIONS);
@@ -134,7 +130,7 @@
           scope.sortableOptions = _.assign({}, SORTABLE_OPTIONS, {onSort: handleSort});
           scope.tree = new Tree(scope.treeNodes, scope.template, scope.buttons);
 
-          initialiseModel();
+          scope.model = Utils.setModel(scope.isMultiSelect, scope.model);
 
           scope.containerStyle = !scope.inline ? { 'overflow': 'scroll', 'max-height': '400px' } : {};
         }
@@ -177,7 +173,7 @@
         scope.select = function(node) {
           //make sure that scope.model is setup
           if(_.isUndefined(scope.model)) {
-            initialiseModel();
+            scope.model = Utils.setModel(scope.isMultiSelect, scope.model);
           }
 
           if(node[scope.tree.template.id]) {
@@ -295,7 +291,7 @@
 
   }
 
-  sequoiaTreeDirective.$inject = ['SequoiaTree', 'BUTTONS', 'DEFAULT_OPTIONS', 'SORTABLE_OPTIONS'];
+  sequoiaTreeDirective.$inject = ['SequoiaTree', 'SequoiaTreeUtils', 'BUTTONS', 'DEFAULT_OPTIONS', 'SORTABLE_OPTIONS'];
 
   angular.module('ngSequoia')
     .directive('sequoiaTree', sequoiaTreeDirective);
@@ -304,153 +300,15 @@
 (function() {
   'use strict';
 
-  function SequoiaTreeFactory($log, NODE_TEMPLATE, BUTTONS) {
-
-    var _checkNodeStructure, _exists, _contains, _buildBreadCrumbs, _buildPath, _selected, _createNodeWithFullPathAsTitle, _guid, _getParentNode, _pruneTree;
+  function SequoiaTreeFactory($log, Utils, NODE_TEMPLATE, BUTTONS) {
 
     var SequoiaTree = function(tree, template, buttons) {
       this.template = template || NODE_TEMPLATE;
-      this.tree = _checkNodeStructure(_.isArray(tree) ? tree[0] : {}, this.template) ? tree : [];
+      this.tree = Utils.checkNodeStructure(_.isArray(tree) ? tree[0] : {}, this.template) ? tree : [];
       this.pagination = {};
       this.buttons = buttons || BUTTONS;
 
       this.resetPagination();
-    };
-
-    _guid = function() {
-      // https://gist.github.com/jed/982883
-      var b = function(a) {return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b);}; // jshint ignore:line
-
-      return b();
-    };
-
-    _checkNodeStructure = function(node, template) {
-      if(!node) {
-        return false;
-      }
-
-      var keys = _.values(template);
-      for(var i=0;i<keys.length;i++) {
-        if (!_.has(node, keys[i])) {
-          $log.warn('The node structure is not valid!');
-          return false;
-        }
-      }
-
-      return true;
-    };
-
-    _exists = function (nodes, key, value, template) {
-      if (_.isArray(nodes)) {
-        return _.some(nodes, function(node){
-          return node[key] === value ? true : _exists(node[template.nodes], key, value, template);
-        });
-      } else {
-        return false;
-      }
-    };
-
-    _contains = function (nodes, key, value, results, template) {
-      if (_.isArray(nodes)) {
-        for(var i=0;i<nodes.length;i++) {
-          if(nodes[i][key].toLowerCase().indexOf(value.toLowerCase()) > -1) {
-            results.push(nodes[i]);
-          }
-          _contains(nodes[i][template.nodes], key, value, results, template);
-        }
-      }
-
-      return results;
-    };
-
-    _buildBreadCrumbs = function(id, nodes, breadcrumbs, template, rootText) {
-      nodes = nodes || [];
-      var root = {};
-      root[template.title] = rootText;
-      breadcrumbs = !breadcrumbs.length ? [root] : breadcrumbs;
-
-      for(var i=0;i<nodes.length;i++) {
-        if(nodes[i][template.id] === id || _exists(nodes[i][template.nodes], template.id, id, template)) {
-          breadcrumbs.push(nodes[i]);
-        }
-        _buildBreadCrumbs(id, nodes[i][template.nodes], breadcrumbs, template, rootText);
-      }
-
-      return breadcrumbs;
-    };
-
-    _buildPath = function(node, nodes, path, template, rootText) {
-      nodes = nodes || [];
-
-      for(var i=0;i<nodes.length;i++) {
-        if(nodes[i][template.id] === node[template.id] || _exists(nodes[i][template.nodes], template.id, node[template.id], template)) {
-          path += path.length ? ' » ' : 'Full path: ' + rootText + ' » ';
-          path += nodes[i][template.title];
-        }
-        path = _buildPath(node, nodes[i][template.nodes], path, template, rootText);
-      }
-
-      return path;
-    };
-
-    _selected = function(id, nodes, selected, template) {
-      if(_.isArray(nodes)) {
-        for(var i=0;i<nodes.length;i++) {
-          if(nodes[i]._id === id) {
-            selected.push(nodes[i]);
-          }
-          _selected(id, nodes[i][template.nodes], selected, template);
-        }
-      }
-
-      return selected;
-    };
-
-    _getParentNode = function(id, nodes, template, breadcrumbs) {
-      nodes = nodes || [];
-
-      breadcrumbs = breadcrumbs || [];
-
-      for(var i=0;i<nodes.length;i++) {
-        if(nodes[i][template.id] === id || _exists(nodes[i][template.nodes], template.id, id, template)) {
-          breadcrumbs.push(nodes[i]);
-        }
-        _getParentNode(id, nodes[i][template.nodes], template, breadcrumbs);
-      }
-
-      return breadcrumbs;
-    };
-
-    _createNodeWithFullPathAsTitle = function(node, tree, template, rootText) {
-      var result = {};
-
-      result[template.id] = node[template.id];
-      result[template.title] = node[template.title];
-      /* This is a bit hacky, will fix */
-      result.fullpath = '<span class="help-text mute">' + _buildPath(node, tree, '', template, rootText) + '</span>';
-      if(_.isArray(node[template.nodes]) && node[template.nodes].length > 0) {
-        result[template.nodes] = node[template.nodes];
-      }
-
-      return result;
-    };
-
-    _pruneTree = function(nodes, id, template) {
-      for (var i=0;i<nodes.length;i++) {
-        var node = nodes[i];
-        if (node[template.id] === id) {
-          nodes.splice(i, 1);
-          return nodes;
-        }
-        if (node[template.nodes]) {
-          if (_pruneTree(node[template.nodes], id, template)) {
-            if (node[template.nodes].length === 0) {
-              delete node[template.nodes];
-            }
-            return nodes;
-          }
-        }
-      }
     };
 
     SequoiaTree.prototype.findParentNode = function(path) {
@@ -458,7 +316,7 @@
     };
 
     SequoiaTree.prototype.buildPathToNode = function(node) {
-      return _buildPath(node, this.tree, '', this.template, this.buttons.root);
+      return Utils.buildPath(node, this.tree, '', this.template, this.buttons.root);
     };
 
     SequoiaTree.prototype.paginate = function() {
@@ -506,14 +364,14 @@
     };
 
     SequoiaTree.prototype.breadcrumbs = function(id) {
-      return _buildBreadCrumbs(id,this.tree,[], this.template, this.buttons.root);
+      return Utils.buildBreadCrumbs(id,this.tree,[], this.template, this.buttons.root);
     };
 
     SequoiaTree.prototype.find = function(key,value) {
       var results = [],
-          found = _contains(this.tree, key, value, [], this.template);
+          found = Utils.contains(this.tree, key, value, [], this.template);
       for(var i=0;i<found.length;i++) {
-        results.push(_createNodeWithFullPathAsTitle(found[i], this.tree,this.template, this.buttons.root));
+        results.push(Utils.createNodeWithFullPathAsTitle(found[i], this.tree,this.template, this.buttons.root));
       }
 
       return results;
@@ -525,15 +383,15 @@
 
       if(_.isArray(obj)) {
         for(var i=0;i<obj.length;i++) {
-          selected = _.union(selected, _selected(obj[i], this.tree, [], this.template));
+          selected = _.union(selected, Utils.selected(obj[i], this.tree, [], this.template));
         }
 
         for(var j=0;j<selected.length;j++) {
-          results.push(_createNodeWithFullPathAsTitle(selected[j], this.tree,this.template, this.buttons.root));
+          results.push(Utils.createNodeWithFullPathAsTitle(selected[j], this.tree,this.template, this.buttons.root));
         }
       } else if(_.isString(obj)) {
-        selected = _selected(obj, this.tree, [], this.template);
-        results.push(_createNodeWithFullPathAsTitle(selected[0], this.tree, this.template, this.buttons.root));
+        selected = Utils.selected(obj, this.tree, [], this.template);
+        results.push(Utils.createNodeWithFullPathAsTitle(selected[0], this.tree, this.template, this.buttons.root));
       } else {
         $log.warn('You must pass an array of IDs or a single ID in order to find the selected nodes!');
       }
@@ -544,7 +402,7 @@
     SequoiaTree.prototype.addNode = function() {
       var node = {};
 
-      node[this.template.id] = _guid();
+      node[this.template.id] = Utils.guid();
       node[this.template.title] = '';
       node[this.template.nodes] = [];
 
@@ -556,16 +414,193 @@
       this.currentNodes = _.without(this.currentNodes, node);
       this.nodes = _.without(this.nodes, node);
       //remove node from the tree, search recursively
-      this.tree = _pruneTree(this.tree, node[this.template.id], this.template);
+      this.tree = Utils.pruneTree(this.tree, node[this.template.id], this.template);
     };
 
     return SequoiaTree;
   }
 
-  SequoiaTreeFactory.$inject = ['$log', 'NODE_TEMPLATE', 'BUTTONS'];
+  SequoiaTreeFactory.$inject = ['$log', 'SequoiaTreeUtils', 'NODE_TEMPLATE', 'BUTTONS'];
 
   angular.module('ngSequoia')
     .factory('SequoiaTree', SequoiaTreeFactory);
+
+})();
+
+(function() {
+  'use strict';
+
+  function SequoiaTreeUtils($log) {
+
+    var service = {};
+
+    service.guid = function() {
+      // https://gist.github.com/jed/982883
+      var b = function(a) {return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b);}; // jshint ignore:line
+
+      return b();
+    };
+
+    service.checkNodeStructure = function(node, template) {
+      if(!node) {
+        return false;
+      }
+
+      var keys = _.values(template);
+      for(var i=0;i<keys.length;i++) {
+        if (!_.has(node, keys[i])) {
+          $log.warn('The node structure is not valid!');
+          return false;
+        }
+      }
+
+      return true;
+    };
+
+    service.exists = function (nodes, key, value, template) {
+      if (_.isArray(nodes)) {
+        return _.some(nodes, function(node){
+          return node[key] === value ? true : service.exists(node[template.nodes], key, value, template);
+        });
+      } else {
+        return false;
+      }
+    };
+
+    service.contains = function (nodes, key, value, results, template) {
+      if (_.isArray(nodes)) {
+        for(var i=0;i<nodes.length;i++) {
+          if(nodes[i][key].toLowerCase().indexOf(value.toLowerCase()) > -1) {
+            results.push(nodes[i]);
+          }
+          service.contains(nodes[i][template.nodes], key, value, results, template);
+        }
+      }
+
+      return results;
+    };
+
+    service.buildBreadCrumbs = function(id, nodes, breadcrumbs, template, rootText) {
+      nodes = nodes || [];
+      var root = {};
+      root[template.title] = rootText;
+      breadcrumbs = !breadcrumbs.length ? [root] : breadcrumbs;
+
+      for(var i=0;i<nodes.length;i++) {
+        if(nodes[i][template.id] === id || service.exists(nodes[i][template.nodes], template.id, id, template)) {
+          breadcrumbs.push(nodes[i]);
+        }
+        service.buildBreadCrumbs(id, nodes[i][template.nodes], breadcrumbs, template, rootText);
+      }
+
+      return breadcrumbs;
+    };
+
+    service.buildPath = function(node, nodes, path, template, rootText) {
+      nodes = nodes || [];
+
+      for(var i=0;i<nodes.length;i++) {
+        if(nodes[i][template.id] === node[template.id] || service.exists(nodes[i][template.nodes], template.id, node[template.id], template)) {
+          path += path.length ? ' » ' : 'Full path: ' + rootText + ' » ';
+          path += nodes[i][template.title];
+        }
+        path = service.buildPath(node, nodes[i][template.nodes], path, template, rootText);
+      }
+
+      return path;
+    };
+
+    service.selected = function(id, nodes, selected, template) {
+      if(_.isArray(nodes)) {
+        for(var i=0;i<nodes.length;i++) {
+          if(nodes[i]._id === id) {
+            selected.push(nodes[i]);
+          }
+          service.selected(id, nodes[i][template.nodes], selected, template);
+        }
+      }
+
+      return selected;
+    };
+
+    service.getParentNode = function(id, nodes, template, breadcrumbs) {
+      nodes = nodes || [];
+
+      breadcrumbs = breadcrumbs || [];
+
+      for(var i=0;i<nodes.length;i++) {
+        if(nodes[i][template.id] === id || service.exists(nodes[i][template.nodes], template.id, id, template)) {
+          breadcrumbs.push(nodes[i]);
+        }
+        service.getParentNode(id, nodes[i][template.nodes], template, breadcrumbs);
+      }
+
+      return breadcrumbs;
+    };
+
+    service.createNodeWithFullPathAsTitle = function(node, tree, template, rootText) {
+      var result = {};
+
+      result[template.id] = node[template.id];
+      result[template.title] = node[template.title];
+      /* This is a bit hacky, will fix */
+      result.fullpath = '<span class="help-text mute">' + service.buildPath(node, tree, '', template, rootText) + '</span>';
+      if(_.isArray(node[template.nodes]) && node[template.nodes].length > 0) {
+        result[template.nodes] = node[template.nodes];
+      }
+
+      return result;
+    };
+
+    service.pruneTree = function(nodes, id, template) {
+      for (var i=0;i<nodes.length;i++) {
+        var node = nodes[i];
+        if (node[template.id] === id) {
+          nodes.splice(i, 1);
+          return nodes;
+        }
+        if (node[template.nodes]) {
+          if (service.pruneTree(node[template.nodes], id, template)) {
+            if (node[template.nodes].length === 0) {
+              delete node[template.nodes];
+            }
+            return nodes;
+          }
+        }
+      }
+    };
+
+    service.setModel = function(isMultiSelect, model) {
+      if(isMultiSelect) {
+        if(_.isArray(model)) {
+          return model;
+        } else if(_.isString(model)) {
+          $log.warn('You passed a string to a multi-select model, but the model is expected to be an array!');
+          return [model];
+        } else {
+          $log.warn('The model is expected to be an array for a multi-select tree!');
+          return [];
+        }
+      } else {
+        if(_.isString(model)) {
+          return model;
+        } else if(_.isArray(model)) {
+          $log.warn('You passed an array to a single-select model, but the model is expected to be a string!');
+          return model[0];
+        } else {
+          $log.warn('The model is expected to be a string for a single-select tree!');
+          return '';
+        }
+      }
+    };
+
+    return service;
+  }
+
+  SequoiaTreeUtils.$inject = ['$log'];
+
+  angular.module('ngSequoia')
+    .service('SequoiaTreeUtils', SequoiaTreeUtils);
 
 })();
 
